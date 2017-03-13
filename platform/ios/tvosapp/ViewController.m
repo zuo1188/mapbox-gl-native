df62ccf8f0c5b52a8dca0725c2d8c04307644b53
Minh Nguyễn <mxn@1ec5.org>
2017-03-13 04:14:59 -0700

    [tvos] Added demo application

---
+++ platform/ios/tvosapp/ViewController.m
@@ -0,0 +1,19 @@
+#import "ViewController.h"
+
+#import "AppDelegate.h"
+
+#import <Mapbox/Mapbox.h>
+
+@interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet MGLMapView *mapView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
