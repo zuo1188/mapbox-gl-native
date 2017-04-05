9ccffa6a6739efe0d1252705809d0ea7eeb9c6c8
Dane Springmeyer <dane@mapbox.com>
2017-04-04 19:42:45 -0700

    update to vector-tile 1.0.0-rc5

--- cmake/core.cmake
+++ cmake/core.cmake
@@ -23,7 +23,7 @@ target_add_mason_package(mbgl-core PRIVATE supercluster)
 target_add_mason_package(mbgl-core PRIVATE kdbush)
 target_add_mason_package(mbgl-core PRIVATE earcut)
 target_add_mason_package(mbgl-core PRIVATE protozero)
-target_add_mason_package(mbgl-core PRIVATE vectortile)
+target_add_mason_package(mbgl-core PRIVATE vector-tile)
 target_add_mason_package(mbgl-core PRIVATE polylabel)
 target_add_mason_package(mbgl-core PRIVATE wagyu)
 
